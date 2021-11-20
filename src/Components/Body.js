4756e3a2e84ed2c32359c868d2b202c594493f77
Drasenberg <macoel14@gmail.com>
2021-11-20 01:55:13 +0100

    Application Completed

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -6,12 +6,20 @@ import iconMessages from "../images/icon-messages.svg";
 import flowingConversation from "../images/illustration-flowing-conversation.svg";
 import growTogether from "../images/illustration-grow-together.svg";
 import yourUsers from "../images/illustration-your-users.svg";
+import bgSectionTopDesktop1 from "../images/bg-section-top-desktop-1.svg";
+import bgSectionTopMobile1 from "../images/bg-section-top-desktop-1.svg";
+import bgSectionBottomDesktop1 from "../images/bg-section-bottom-desktop-1.svg";
+import bgSectionBottomMobile1 from "../images/bg-section-bottom-mobile-1.svg";
+import bgSectionTopDesktop2 from "../images/bg-section-top-desktop-2.svg";
+import bgSectionTopMobile2 from "../images/bg-section-top-desktop-2.svg";
+import bgSectionBottomDesktop2 from "../images/bg-section-bottom-desktop-2.svg";
+import bgSectionBottomMobile2 from "../images/bg-section-bottom-mobile-2.svg";
 
 function Body() {
   return (
     <div className={classes.body}>
       <div className={classes.topSection}>
-        <h2>Build The Community Your Fans Will Love</h2>
+        <h1>Build The Community Your Fans Will Love</h1>
         <p>
           Huddle re-imagines the way we build communities. You have a voice, but
           so does your audience. Create connections with your users as you
@@ -21,46 +29,102 @@ function Body() {
       </div>
       <div className={classes.mainSection}>
         <img src={mockupScreen} alt="mockupScreen" />
-        <p>
-          <img src={iconCommunities} alt="iconCommunities" />
-          <span>1.4k+</span>Communities Formed
-        </p>
-        <p>
-          <img src={iconMessages} alt="iconMessages" />
-          <span>2.7m+</span>Messages Sent
-        </p>
+        <div className={classes.diagram}>
+          <p>
+            <img src={iconCommunities} alt="iconCommunities" />
+            <span>1.4k+</span>Communities Formed
+          </p>
+          <p>
+            <img src={iconMessages} alt="iconMessages" />
+            <span>2.7m+</span>Messages Sent
+          </p>
+        </div>
       </div>
       <section className={classes.articles}>
         <article className={classes.growTogether}>
+          <img
+            src={bgSectionTopDesktop1}
+            alt=""
+            className={classes.bgSectionTopDesktop}
+          />
+          <img
+            src={bgSectionTopMobile1}
+            alt=""
+            className={classes.bgSectionTopMobile}
+          />
           <div className={classes.container}>
-            <img src={growTogether} alt="growTogether" />
-            <h1>Grow Together</h1>
-            <p>
-              Generate meaningful discussions with your audience and build a
-              strong, loyal community. Think of the insightful conversations you
-              miss out on with a feedback form.
-            </p>
+            <div className={classes.imageContainer}>
+              <img src={growTogether} alt="growTogether" />
+            </div>
+            <div className={classes.textContainer}>
+              <h1>Grow Together</h1>
+              <p>
+                Generate meaningful discussions with your audience and build a
+                strong, loyal community. Think of the insightful conversations
+                you miss out on with a feedback form.
+              </p>
+            </div>
           </div>
+          <img
+            src={bgSectionBottomDesktop1}
+            alt=""
+            className={classes.bgSectionBottomDesktop}
+          />
+          <img
+            src={bgSectionBottomMobile1}
+            alt=""
+            className={classes.bgSectionBottomMobile}
+          />
         </article>
         <article className={classes.flowingConversation}>
-          <img src={flowingConversation} alt="flowingConversation" />
-          <h1>Flowing Conversations</h1>
-          <p>
-            You wouldn't paginate a conversation in real life, so why do it
-            online? Our threads have just-in-time loading for a more natural
-            flow.
-          </p>
+          <div className={classes.flowingConversationContainer}>
+            <div className={classes.imageContainer}>
+              <img src={flowingConversation} alt="flowingConversation" />
+            </div>
+            <div className={classes.textContainer}>
+              <h1>Flowing Conversations</h1>
+              <p>
+                You wouldn't paginate a conversation in real life, so why do it
+                online? Our threads have just-in-time loading for a more natural
+                flow.
+              </p>
+            </div>
+          </div>
         </article>
         <article className={classes.yourUsers}>
+          <img
+            src={bgSectionTopDesktop2}
+            alt=""
+            className={classes.bgSectionTopDesktop}
+          />
+          <img
+            src={bgSectionTopMobile2}
+            alt=""
+            className={classes.bgSectionTopMobile}
+          />
           <div className={classes.container}>
-            <img src={yourUsers} alt="yourUsers" />
-            <h1>Your Users</h1>
-            <p>
-              It takes no time at all to integrate Huddle with your app's
-              authentication solution. This means, once signed in to your app,
-              your users can start chatting immediately.
-            </p>
+            <div className={classes.imageContainer}>
+              <img src={yourUsers} alt="yourUsers" />
+            </div>
+            <div className={classes.textContainer}>
+              <h1>Your Users</h1>
+              <p>
+                It takes no time at all to integrate Huddle with your app's
+                authentication solution. This means, once signed in to your app,
+                your users can start chatting immediately.
+              </p>
+            </div>
           </div>
+          <img
+            src={bgSectionBottomDesktop2}
+            alt=""
+            className={classes.bgSectionBottomDesktop}
+          />
+          <img
+            src={bgSectionBottomMobile2}
+            alt=""
+            className={classes.bgSectionBottomMobile}
+          />
         </article>
         <div className={classes.buildCommunity}>
           <h1>Ready To Build Your Community?</h1>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,34 +3,52 @@ import classes from "../Styles/Footer.module.scss";
 import Logo from "../images/logo.svg";
 import iconEmail from "../images/icon-email.svg";
 import iconPhone from "../images/icon-phone.svg";
+import facebookIcon from '../images/facebook-square-brands.svg';
+import instagramIcon from '../images/instagram-brands.svg';
+import twitterIcon from '../images/twitter-square-brands.svg';
+import bgFooterTopMobile from '../images/bg-footer-top-mobile.svg';
+import bgFooterTopDesktop from '../images/bg-footer-top-desktop.svg';
 
 function Footer() {
   return (
+    <div className={classes.allFooter}>
+      <img src={bgFooterTopMobile} alt="" className={classes.bgFooterMobile} />
+      <img src={bgFooterTopDesktop} alt="" className={classes.bgFooterDesktop} />
     <div className={classes.footer}>
-      <div className={classes.newsletter}>
-        <h1>Newsletter</h1>
-        <p>
-          To recieve tips on how to grow your community, sign up to our weekly
-          newsletter. We’ll never send you spam or pass on your email address
-        </p>
-        <input type="email" name="email" />
-        <button>Subscribe</button>
-      </div>
-      <div className={classes.contact}>
-        <img src={Logo} alt="Logo" />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla
-          quam, hendrerit lacinia vestibulum a, ultrices quis sem.
-        </p>
-        <p className={classes.phone}>
-          <img src={iconPhone} alt="phone" />
-          Phone: +1-543-123-4567
-        </p>
-        <p className={classes.email}>
-          <img src={iconEmail} alt="email" />
-          example@huddle.com
-        </p>
-        <div className={classes.social}></div>
+      <div className={classes.container}>
+        <div className={classes.newsletter}>
+          <div className={classes.newsletterContent}>
+            <h1>Newsletter</h1>
+            <p>
+              To recieve tips on how to grow your community, sign up to our weekly
+              newsletter. We’ll never send you spam or pass on your email address
+            </p>
+          </div>
+          <div className={classes.subscription}>
+            <input type="email" name="email" />
+            <button>Subscribe</button>
+          </div>
+        </div>
+        <div className={classes.contact}>
+          <img src={Logo} alt="Logo" className={classes.logo} />
+          <p className={classes.textToLogo}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla
+            quam, hendrerit lacinia vestibulum a, ultrices quis sem.
+          </p>
+          <p className={classes.phone}>
+            <img src={iconPhone} alt="phone" />
+            <p>Phone: +1-543-123-4567</p>
+          </p>
+          <p className={classes.email}>
+            <img src={iconEmail} alt="email" />
+            <p>example@huddle.com</p>
+          </p>
+          <div className={classes.social}>
+            <img src={facebookIcon} alt="facebook" className={classes.facebook}/>
+            <img src={instagramIcon} alt="instagram" className={classes.instagram} />
+            <img src={twitterIcon} alt="twitter" className={classes.twitter} />
+          </div>
+        </div>
       </div>
       <footer>
         <p class={classes.attribution}>
@@ -38,9 +56,10 @@ function Footer() {
           <a href="https://www.frontendmentor.io?ref=challenge">
             Frontend Mentor
           </a>
-          . Coded by <a href="/">Your Name Here</a>.
+          . Coded by <a href="https://github.com/Drasenberg/huddle-landing-page-with-curved-sections">Maciej Woźniak</a>.
         </p>
       </footer>
+    </div>
     </div>
   );
 }
